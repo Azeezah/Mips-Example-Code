654e9451c12f98a874a935833bd9958bbb10ca76
Azeezah Muhammad <azeezahm14@gmail.com>
2018-12-13 14:20:29 -0500

    Pass the parameters on the stack

--- sum with params on stack.s
+++ sum with params on stack.s
@@ -1,6 +1,7 @@
 .data
-numbers: .byte 1 2 3 4 5
-sum_header: .asciiz "array sum: "
+	numbers: .byte 1 2 3 4 5
+	sum_header: .asciiz "array sum: "
+
 .text
 
 main:
@@ -10,9 +11,12 @@ main:
 	li $v0, 4	#print sum_header
 	la $a0, sum_header
 	syscall
-	
-	la $a0, numbers	#set arr addr
-	li $a1, 5	#set arr length
+
+	la $a0, numbers	#get arr addr
+	li $a1, 5	#get arr length
+	addi $sp, $sp, -8	#allocate space for parameters
+	sw $a0, 0($sp)		#store parameters
+	sw $a1, 4($sp)
 	jal sum
 	move $t0, $v0	#get return value
 
@@ -26,6 +30,10 @@ main:
 
 
 sum:		#function args: $a0 = array_addr, $a1 = array_len
+	lw $a0, 0($sp)
+	lw $a1, 4($sp)		
+	addi $sp, $sp, 8	#deallocate space for parameters
+
 	addi $sp, $sp, -8
 	sw $ra, 0($sp)
 	sw $s0, 4($sp)
@@ -39,6 +47,9 @@ dont_return_first:
 
 	addi $a0, $a0, 1	#increment addr, effectively removing first element from array
 	addi $a1, $a1, -1	#decrement length accordingly
+	addi $sp, $sp, -8	#allocate space for parameters
+	sw $a0, 0($sp)		#store parameters
+	sw $a1, 4($sp)
 	jal sum
 	add $v0, $s0, $v0	#return first number + sum of the rest
 	#the first $v0 on this line contains the function return value, and the second $v0 contains the return value from the recursive call
